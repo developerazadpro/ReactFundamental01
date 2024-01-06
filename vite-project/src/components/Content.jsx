fa924305aa8e0d5a9a01ec3ee574b17dba514349
developerazadpro <azharul.ece13.hstu@gmail.com>
2024-01-06 23:59:39 +0600

    component making

---
+++ vite-project/src/components/Content.jsx
@@ -0,0 +1,10 @@
+const Content = () => {
+    return (
+        <div>
+            <h1>Heading</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis praesentium voluptate corrupti facilis sequi cumque maiores saepe facere culpa labore. Pariatur obcaecati labore dolorum ad, adipisci architecto eum rerum saepe?</p>
+        </div>
+    );
+};
+
+export default Content;
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Header;
